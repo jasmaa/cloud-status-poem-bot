f326c0b3bec9be6acc87c0c5bc0dc7753f1c7927
jasmaa <jasonmaa3955@gmail.com>
2024-11-23 08:46:38 -0500

    chore: Port generate-wrangler script to js

---
+++ scripts/generate-wrangler.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const fs = require('node:fs');
+
+const content = `name = "aws-status-poem-bot"
+compatibility_date = "2023-07-23"
+
+[[kv_namespaces]]
+binding = "FEED_ITEMS"
+id = "${process.env.FEED_ITEMS_ID}"
+preview_id = "${process.env.FEED_ITEMS_PREVIEW_ID}"
+
+[triggers]
+crons = ["* * * * *"]
+
+[vars]
+MSTDN_URL = "${process.env.MSTDN_URL}"
+
+# Secrets
+# - GEMINI_API_KEY
+# - MSTDN_ACCESS_TOKEN`;
+
+fs.writeFileSync('wrangler.toml', content);
